26d7a54b15fce75c437949890d9b949eeaa5f501
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-06-19 22:19:53 -0700

    Refactor views with partial form

--- W4D2/Library/app/controllers/books_controller.rb
+++ W4D2/Library/app/controllers/books_controller.rb
@@ -15,16 +15,32 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book = Book.new
     render :new
   end
 
   def create
     @book = Book.new(book_params)
 
-    if @book.save!
-      redirect_to book_url(@book.id)
+    if @book.save
+      redirect_to book_url(@book)
     else
       render :new
+    end
+  end
+
+  def edit
+    @book = Book.find_by(id: params[:id])
+    render :edit
+  end
+
+  def update
+    @book = Book.find_by(id: params[:id])
+
+    if @book.update_attributes(book_params)
+      redirect_to book_url(@book)
+    else
+      render :edit
     end
   end
 
