d80ae2f707b36eb2c540114e75cc140956d0b38d
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-07-11 23:21:20 -0700

    Add giphy presentational component and container

--- W7D2/components/giphys_search.jsx
+++ W7D2/components/giphys_search.jsx
@@ -1,3 +1,43 @@
 import React from 'react';
 
 import GiphysIndex from './giphys_index';
+
+class GiphysSearch extends React.Component {
+  constructor() {
+    super();
+    this.state = { searchTerm: 'golden retriever' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  render() {
+    let { giphys } = this.props;
+
+    return (
+      <div>
+        <form className="search-bar">
+          <input value={this.state.searchTerm} onChange={this.handleChange} />
+          <button type="submit" onClick={this.handleSubmit}>Search for Gif!</button>
+        </form>
+
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    this.props.fetchSearchGiphys('golden+retriever');
+  }
+
+  handleChange(e) {
+    this.setState({ searchTerm: e.currentTarget.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    let searchTerm = this.state.searchTerm.split(' ').join('+');
+    this.props.fetchSearchGiphys(searchTerm);
+  }
+
+}
+
+export default GiphysSearch;
